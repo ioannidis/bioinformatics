7829ea531563b9bb8f453d0b2f11918285ccf8ba
Thanos Paravantis <thanosparavantis@gmail.com>
2019-06-23 17:50:08 +0300

    Update LCS

--- exercise-6_22.py
+++ exercise-6_22.py
@@ -2,32 +2,55 @@ import numpy as np
 
 
 def lcs(v, w):
-    width = len(v)
-    height = len(w)
-    s = np.zeros((width, height))
-    b = np.empty((width, height), dtype='U4')
+    n = len(v)
+    m = len(w)
 
-    for i in range(width):
-        for j in range(height):
+    s = [[0 for j in range(m)] for i in range(n)]
+    b = [['' for j in range(m)] for i in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            print(f'Comparing {v[i]} with {w[j]}')
+
             if v[i] == w[j]:
                 s[i][j] = s[i - 1][j - 1] + 1
             else:
-                s[i][j] = max(s[i - 1][j], s[i, j - 1])
+                s[i][j] = max(s[i - 1][j], s[i][j - 1])
 
             if s[i][j] == s[i - 1][j]:
-                b[i][j] = '\2191'
+                b[i][j] = '\u2191'
             elif s[i][j] == s[i][j - 1]:
-                b[i][j] = '\2190'
-            elif s[i][j] == s[i - 1][j - 1]:
-                b[i][j] = '\2196'
+                b[i][j] = '\u2190'
+            elif s[i][j] == s[i - 1][j - 1] + 1:
+                b[i][j] = '\u2196'
     return s, b
+
+
+def printLcs(b, v, i, j):
+    pass
 
 
 if __name__ == '__main__':
     v = 'TGCATA'
     w = 'ATCTGAT'
 
-    print(v, w)
+    print('Sequence v:')
+    print(v)
+
+    print()
+
+    print('Sequence w:')
+    print(w)
+
+    print()
 
     s, b = lcs(v, w)
-    print(s)
+    print('Matrix s:')
+    for row in s:
+        print(row)
+
+    print()
+
+    print('Matrix b:')
+    for row in b:
+        print(row)
