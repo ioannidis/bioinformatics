0df9b882b90420fefa291f260a09e72424c4baf8
Thanos Paravantis <thanosparavantis@gmail.com>
2019-06-23 17:58:26 +0300

    Add 6.23, 6.37 files

---
+++ exercise-6_23.py
@@ -0,0 +1,9 @@
+# ============ CS UNIPI =============#
+#       BioInformatics 2018-19      #
+# ===================================#
+#   P16036 - Ioannidis Panagiotis   #
+#   P16112 - Paravantis Athanasios  #
+# ===================================#
+
+if __name__ == '__main__':
+    pass
