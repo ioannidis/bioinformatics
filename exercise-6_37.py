7dd80878d28d8667f31a6fb8bbb4e6edd99b1923
Thanos Paravantis <thanosparavantis@gmail.com>
2019-06-24 15:37:03 +0300

    Update 6.37

--- exercise-6_37.py
+++ exercise-6_37.py
@@ -5,5 +5,30 @@
 #   P16112 - Paravantis Athanasios  #
 # ===================================#
 
+def restore(seq):
+    seq = [char for char in seq]
+    idx_removes = []
+
+    prev = ''
+    for idx, char in enumerate(seq):
+        if len(prev) > 0 and char == prev:
+            idx_removes.append(idx)
+        prev = char
+
+    result = []
+
+    for idx, char in enumerate(seq):
+        if idx in idx_removes:
+            continue
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+
 if __name__ == '__main__':
-    pass
+    before = 'AAATAAAGGGGCCCCCTTTTTTTCC'
+    after = restore(before)
+
+    print(f'Befor: {before}')
+    print(f'After: {after}')
