76142c310b775c005efa06a2f5db4e67f90cd17f
Panos Ioannidis <panos277@hotmail.com>
2019-06-25 19:50:47 +0300

    Update 11.6

--- exercise-11_6.py
+++ exercise-11_6.py
@@ -7,194 +7,90 @@
 
 import numpy as np
 
-# Dice selection probabilities
-dice_selection_prob = [0.5, 0.5]
-
 # Transition matrix
-# [[p(D1|D1) , p(D2|D1) , p(END|D1) ],
-#  [p(D1|D2) , p(D2|D2) , p(END|D2) ],
-#  [p(D1|END), p(D2|END), p(END|END)]]
-transition_matrix = [[0.50, 0.25, 0.25],
-                     [0.25, 0.50, 0.25],
-                     [0.00, 0.00, 0.00]]
+# [[p(START|D1), p(D1|D1) , p(D2|D1) , p(END|D1) ],
+#  [p(START|D2), p(D1|D2) , p(D2|D2) , p(END|D2) ],
+#  [p(START|D3), p(D1|D2) , p(D2|D2) , p(END|D2) ],
+#  [p(START|END), p(D1|END), p(D2|END), p(END|END)]]
+transition_matrix = [[0.00, 0.50, 0.50, 0.00],
+                     [0.00, 0.50, 0.25, 0.25],
+                     [0.00, 0.25, 0.50, 0.25],
+                     [0.00, 0.00, 0.00, 0.00]]
 
-# Transition matrix
-# [[p(1|D1), p(1|D2), p(1|END)],
-#  [p(2|D1), p(2|D2), p(2|END)],
-#  [p(3|D1), p(3|D2), p(3|END)]]
-emission_matrix = [[0.50, 0.25, 0.00],
-                   [0.25, 0.50, 0.00],
-                   [0.25, 0.25, 0.00]]
 
-emissions = ['1', '2', '3']
+# Emission matrix
+# [[p(1|START), p(1|D1), p(1|D2), p(1|END)],
+#  [p(2|START), p(2|D1), p(2|D2), p(2|END)],
+#  [p(3|START), p(3|D1), p(3|D2), p(3|END)]]
+emission_matrix = [[0.00, 0.50, 0.25, 0.00],
+                   [0.00, 0.25, 0.50, 0.00],
+                   [0.00, 0.25, 0.25, 0.00]]
 
 states = ['D1', 'D2']
+states_len = len(states)
 
-# Observation sequence
 observation = '112122'
+observation_len = len(observation)
 
-# Transform probability matrices to log-scores
-with np.errstate(divide='ignore'):
-    transition_matrix_log = np.log2(transition_matrix)
-    transition_matrix_log[np.isneginf(transition_matrix_log)] = 0
+viterbi_matrix = np.zeros((states_len, observation_len), dtype=int)
+backtracking_matrix = np.zeros((states_len, observation_len), dtype=int)
 
-    emission_matrix_log = np.log2(emission_matrix)
-    emission_matrix_log[np.isneginf(emission_matrix_log)] = 0
+# Viterbi algorithm
+for i in range(states_len):
+    viterbi_matrix[i][0] = np.log2(emission_matrix[int(observation[0]) - 1][i + 1]) + np.log2(transition_matrix[0][i + 1])
+    
+counter = 1
+for i in observation[1:]:
 
-    dice_selection_prob_log = np.log2(dice_selection_prob)
+    for j in range(states_len):
 
+        max_node_score = [viterbi_matrix[0][counter - 1] + np.log2(transition_matrix[1][j + 1]), viterbi_matrix[1][counter - 1] + np.log2(transition_matrix[2][j + 1])]
 
-viterbi_array= np.zeros(shape=(len(states), len(observation)))
+        if max_node_score[0] > max_node_score[1]:
+            viterbi_matrix[j][counter] = np.log2(emission_matrix[int(i) - 1][j + 1]) + max_node_score[0]
+        elif max_node_score[0] < max_node_score[1]:
+            viterbi_matrix[j][counter] = np.log2(emission_matrix[int(i) - 1][j + 1]) + max_node_score[1]
+            backtracking_matrix[j][counter] = 1
+        else:
+            viterbi_matrix[j][counter] = np.log2(emission_matrix[int(i) - 1][j + 1]) + max_node_score[0]
+            backtracking_matrix[j][counter] = 2
 
-# Calculate the score at first step and then
-# use dynamic programming to calculate the other steps
-for i in range(len(observation)):
+    counter += 1
 
-    if (i == 0):
-        viterbi_array[0][i] = dice_selection_prob_log[0] + emission_matrix_log[emissions.index(observation[i])][0]
-        viterbi_array[1][i] = dice_selection_prob_log[1] + emission_matrix_log[emissions.index(observation[i])][1]
-    else:
-        temp_scores = []
-        for j in range(len(states)):
-            prev_node_score = viterbi_array[j][i-1]
+# Viterbi backtracking
+best_sequences = ['']
+if viterbi_matrix[0][states_len] > viterbi_matrix[1][states_len]:
+    best_index = [0]
+    best_sequences[0] = states[0]
+else:
+    best_index = [1]
+    best_sequences[0] = states[1]
 
-            temp_scores.append(prev_node_score + transition_matrix_log[states.index('D1')][j] + emission_matrix_log[emissions.index(observation[i])][j])
-            temp_scores.append(prev_node_score + transition_matrix_log[states.index('D2')][j] + emission_matrix_log[emissions.index(observation[i])][j])
-
-        viterbi_array[0][i] = max(temp_scores[0], temp_scores[2])
-        viterbi_array[1][i] = max(temp_scores[1], temp_scores[3])
-
-viterbi_array_len = viterbi_array.shape[1]
-best_score = max(viterbi_array[0][viterbi_array_len - 1], viterbi_array[1][viterbi_array_len - 1])
-
-print("The best score is:", best_score)
-
-
-viterbi_array_reversed = np.flip(viterbi_array, axis=1)
-observation_reversed = observation[::-1]
-print(viterbi_array_reversed)
-print(observation_reversed)
-
-best_sequence = []
-v_best_score_matrix = []
-for i in range(len(observation_reversed)):
-
-    if (i == 0):
-        # viterbi_array[0][i] = dice_selection_prob_log[0] + emission_matrix_log[emissions.index(observation[i])][0]
-        # viterbi_array[1][i] = dice_selection_prob_log[1] + emission_matrix_log[emissions.index(observation[i])][1]
-        if (viterbi_array_reversed[0][i] == best_score - transition_matrix_log[2][0]):
-            best_sequence.append('D1')
-            v_best_score_matrix.append(best_score - transition_matrix_log[2][0])
-
-        if (viterbi_array_reversed[1][i] == best_score - transition_matrix_log[2][1]):
-            best_sequence.append('D2')
-            v_best_score_matrix.append(best_score - transition_matrix_log[2][1])
-
-        # print(viterbi_array_reversed[i][0])
-        # print(viterbi_array_reversed[i][1])
-        # print(best_score - transition_matrix_log[states.index('END')][0])
-        # print(viterbi_array_reversed[i][0] == best_score - transition_matrix_log[states.index('END')][0])
-        # print(best_score - transition_matrix_log[states.index('END')][1])
-        # print(viterbi_array_reversed[i][1] == best_score - transition_matrix_log[states.index('END')][1])
-    else:
-        temp_scores = []
-        # for j in range(len(states)):
-
-        prev_best_score = v_best_score_matrix[i-1]
+for i in range(backtracking_matrix.shape[1] - 1, 0, -1):
+    for j in range(len(best_sequences)):
+        if backtracking_matrix[best_index[j], i] != 2:
+            best_sequences[j] = states[backtracking_matrix[best_index[j]][i]] + '-' + best_sequences[j]
+            best_index[j] = backtracking_matrix[best_index[j]][i]
+        else:
+            best_sequences.append(best_sequences[j])
+            best_index.append(best_index[j])
+            best_sequences[j] = states[0] + '-' + best_sequences[j]
+            best_sequences[j+1] = states[1] + '-' + best_sequences[j+1]
+            best_index[j] = backtracking_matrix[0][i - 1]
+            best_index[j+1] = backtracking_matrix[1][i - 1]
 
 
+# Print viterbi matrix
+print('The viterbi matrix is:')
+print(viterbi_matrix)
 
-        j = states.index(best_sequence[i-1])
+# Print best sequences
+print('\nThe best sequences are:')
+for seq in best_sequences:
+    print(seq)
 
-        a = prev_best_score - (transition_matrix_log[states.index('D1')][j] + emission_matrix_log[emissions.index(observation_reversed[i-1])][j])
-        b = prev_best_score - (transition_matrix_log[states.index('D2')][j] + emission_matrix_log[emissions.index(observation_reversed[i-1])][j])
+# Print best score
+best_index = 0 if viterbi_matrix[0][states_len] > viterbi_matrix[1][states_len] else 1
+print('\nThe best score is:',str(viterbi_matrix[best_index][counter - 1] + np.log2(transition_matrix[best_index + 1][3])))
 
-        if (viterbi_array_reversed[0][i] == a and viterbi_array_reversed[1][i] == b):
-            best_sequence.append('D1')
-            print("*****")
-            v_best_score_matrix.append(a)
-
-        elif (viterbi_array_reversed[0][i] == a):
-            best_sequence.append('D1')
-            v_best_score_matrix.append(a)
-
-        elif (viterbi_array_reversed[1][i] == b):
-            best_sequence.append('D2')
-            v_best_score_matrix.append(b)
-
-
-
-        print(a , b)
-        # break
-
-
-        print(best_sequence)
-        print(v_best_score_matrix)
-
-            # temp_scores.append(prev_node_score + transition_matrix_log[states.index('D1')][j] + emission_matrix_log[emissions.index(observation[i])][j])
-            # temp_scores.append(prev_node_score + transition_matrix_log[states.index('D2')][j] + emission_matrix_log[emissions.index(observation[i])][j])
-    #
-    #     viterbi_array[0][i] = max(temp_scores[0], temp_scores[2])
-    #     viterbi_array[1][i] = max(temp_scores[1], temp_scores[3])
-
-
-# v_max_score_matrix = []
-# j=5
-# if viterbi_array[0][j] > viterbi_array[1][j]:
-#     state = "D1"
-#     v_max_score_matrix.append(viterbi_array[0][j])
-# elif viterbi_array[0][j] < viterbi_array[1][j]:
-#     state = "D2"
-#     v_max_score_matrix.append(viterbi_array[1][j])
-# else:
-#     state = "*"
-#     v_max_score_matrix.append(viterbi_array[1][j])
-
-# for j in range(viterbi_array_reversed.shape[1]-1):
-#     # print(v_max_score_matrix[j] - (transition_matrix_log[states.index('D2')][0] +  emission_matrix_log[emissions.index(observation[j])][0]))
-#     if ( viterbi_array_reversed[0][j] == v_max_score_matrix[0] - (transition_matrix_log[1][0] +  emission_matrix_log[1][0]) ):
-#         print("malakas")
-
-# print(v_max_score_matrix[0] - (transition_matrix_log[1][0] +  emission_matrix_log[1][0]))
-
-    # if viterbi_array[0][j] > viterbi_array[1][j]:
-    #     for seq_index in range(len(best_sequences)):
-    #         best_sequences[seq_index] = best_sequences[seq_index] + "D1"
-    #
-    # if viterbi_array[0][j] < viterbi_array[1][j]:
-    #     for seq_index in range(len(best_sequences)):
-    #         best_sequences[seq_index] = best_sequences[seq_index] + "D2"
-    #
-    # if viterbi_array[0][j] == viterbi_array[1][j]:
-    #     for seq_index in range(len(best_sequences)):
-    #         best_sequences[seq_index] = best_sequences[seq_index] + "*"
-
-        # best_sequences.append(best_sequences[seq_index] + "D2")
-
-
-# print("best_sequences_matrix: ")
-# print(best_sequences)
-# print(v_max_score_matrix)
-# print(viterbi_array_reversed)
-# print(observation_reversed)
-
-#
-# j = viterbi_array.shape[1] - 1
-# if viterbi_array[0][j] > viterbi_array[1][j]:
-#     state = "D1"
-#     v_max_score_matrix.append(viterbi_array[0][j])
-# elif viterbi_array[0][j] < viterbi_array[1][j]:
-#     state = "D2"
-#     v_max_score_matrix.append(viterbi_array[1][j])
-# else:
-#     state = "*"
-#     v_max_score_matrix.append(viterbi_array[1][j])
-#
-# best_sequences_matrix[0].insert(0,state)
-
-
-
-
-
-# print("The best path for the sequence is:", "-".join(best_sequence))
 
